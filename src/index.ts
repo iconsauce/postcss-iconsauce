5bb79d9b6237d3af683dc2d3de8cd303dcdb3599
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-02-27 18:14:26 +0100

    fix: check param if config or path

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import { parse, Plugin } from 'postcss'
+import { PathLike } from 'fs'
 import { build } from '@iconsauce/core'
 import { IconsauceConfig } from '@iconsauce/config'
 import { Config } from '@iconsauce/config/src/interface/config'
 
-const plugin = (configuration?: Config): Plugin => {
-  const config = configuration ?? new IconsauceConfig()
+const plugin = (configOrPath?: Config | PathLike): Plugin => {
   return {
     postcssPlugin: 'postcss-iconsauce',
     async AtRule (rule) {
       if (rule.name === 'iconsauce') {
-        rule.replaceWith(parse(await build(config)))
+        if (typeof configOrPath === 'string' ) {
+          rule.replaceWith(parse(await build(new IconsauceConfig(configOrPath))))
+        } else {
+          rule.replaceWith(parse(await build(configOrPath as Config ?? new IconsauceConfig())))
+        }
       }
     },
   }
